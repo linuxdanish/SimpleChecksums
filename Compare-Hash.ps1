cafae196cfbe703913dcf3f057fbe8587fff8e88
linuxdanish <daniellawrencetate@gmail.com>
2015-10-26 22:34:10 -0400

    Simple PowerShell script that accepts an Algorithm, file to hash, and a hash to compare it to. Note that this script is unsigned

---
+++ Compare-Hash.ps1
@@ -0,0 +1,14 @@
+﻿Param(
+    [Parameter(Mandatory=$True,Position=1)]
+        [string]$Algorithm,
+
+    [Parameter(Mandatory=$True,Position=2)]
+        [string]$Path,
+
+    [Parameter(Mandatory=$True,Position=3)]
+        [string]$inputHash
+)
+$calcHash = Get-FileHash -Algorithm $Algorithm -Path $Path | Select -ExpandProperty Hash
+If ($calcHash -eq $inputHash) {
+    Write-Host "Success! The file's $Algorithm hash equals the same one you provided."
+} else { Write-Host "Crickey! The hashes don't match! If you are sure you provided the proper input hash, then somone or something has messed up your file."}
